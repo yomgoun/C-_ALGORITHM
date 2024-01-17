c141aa930b7161ef08e64d2d221dd6c821090534
yomgoun <154971958+yomgoun@users.noreply.github.com>
2024-01-17 10:05:29 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.13 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int num1, int num2) 
+    {
+       if(num1 == num2)
+       {
+          return 1;
+       }
+       else
+       {
+           return -1;
+        }
+    }
+}
