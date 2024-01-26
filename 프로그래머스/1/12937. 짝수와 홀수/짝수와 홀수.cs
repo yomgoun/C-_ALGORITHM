4a9d1642ea824b22aec3f8503e6330c81062dc20
yomgoun <154971958+yomgoun@users.noreply.github.com>
2024-01-26 12:25:31 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.14 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public string solution(int num) {
+        if (num % 2 == 0)
+        {
+            return "Even";
+        }
+        else
+        {
+            return "Odd";
+        }
+    }
+}
