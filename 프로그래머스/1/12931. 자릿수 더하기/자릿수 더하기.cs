e9cd094a19a6a61af49e859c6219e762e6a36c4e
yomgoun <154971958+yomgoun@users.noreply.github.com>
2024-01-30 14:28:48 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.24 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
